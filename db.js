956b3aa1f5d93b2a4fc8dbe3706a0e718f1429db
chitochuks <fayveechuks@gmail.com>
2022-08-25 13:49:29 +0100

    my app

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const mongoURL = 'mongodb+srv://ChitoChuks:15Wrto22ciPG35CT@chitos.mghlh.mongodb.net/cheetos-pizza'
-
-mongoose.connect(mongoURL, {useUnifiedTopology:true, useNewURLParser:true })
 
 var db = mongoose.connection
 
-db.on('connected' , () => {
-    console.log('Mongo DB Connection Successful');
+    mongoose.connect(process.env.mongoURL, {useUnifiedTopology:true, useNewURLParser:true });
 
-})
+    db.on('connected' , () => {
+        console.log('Mongo DB Connection Successful');
+    
+    })
+    
+    db.on('error', () => {
+        console.log('Mongo DB Connection Failed');
+    })
 
-db.on('error', () => {
-    console.log('Mongo DB Connection Failed');
-})
 
 module.exports = mongoose
